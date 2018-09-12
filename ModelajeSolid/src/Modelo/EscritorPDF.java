57dbefd6ed6fc9367163542abe95230c92d27f31
lelthar <geraldm1998@hotmail.com>
2018-09-11 23:15:13 -0600

    Se agregó el escritor de pdf

--- ModelajeSolid/src/Modelo/EscritorPDF.java
+++ ModelajeSolid/src/Modelo/EscritorPDF.java
@@ -5,6 +5,15 @@
  */
 package Modelo;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author josed
@@ -12,7 +21,27 @@ package Modelo;
 public class EscritorPDF implements DTOEscritor {
     
     public boolean Escribir(String Dto){
-        return true;
+        //Obtiene la fecha actual en el sistema
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");  
+        Date date = new Date();  
+        
+        Document document = new Document();
+        try
+        {
+           //Crea la variable del tipo pdf y le pasa el nombre que tendrá el pdf, el cual será la hora y fecha del sistema.
+           PdfWriter writer_pdf = PdfWriter.getInstance(document, new FileOutputStream(formatter.format(date)+".pdf"));
+           document.open();
+           document.add(new Paragraph(Dto)); //Agrega el texto al parrafo del documento pdf
+           document.close();
+           writer_pdf.close();
+           return true;
+        } catch (DocumentException e)
+        {
+           return false;
+        } catch (FileNotFoundException e)
+        {
+           return false;
+        }
     }
 
     
