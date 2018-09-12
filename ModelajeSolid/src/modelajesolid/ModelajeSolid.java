da25758e74d19dc01d6c40cf35762347ac65fb36
lelthar <geraldm1998@hotmail.com>
2018-09-11 22:58:54 -0600

    pdf agregado

--- ModelajeSolid/src/modelajesolid/ModelajeSolid.java
+++ ModelajeSolid/src/modelajesolid/ModelajeSolid.java
@@ -5,6 +5,13 @@
  */
 package modelajesolid;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 /**
  *
  * @author josed
@@ -14,8 +21,24 @@ public class ModelajeSolid {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
+        Document document = new Document();
+        try
+        {
+           PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+           document.open();
+           document.add(new Paragraph("Hello word"));
+           document.close();
+           writer.close();
+        } catch (DocumentException e)
+        {
+           e.printStackTrace();
+        } catch (FileNotFoundException e)
+        {
+           e.printStackTrace();
+        }
+     
     }
     
 }
