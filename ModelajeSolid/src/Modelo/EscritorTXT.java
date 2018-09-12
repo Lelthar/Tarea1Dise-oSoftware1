880deef00f8c6d8b9a55908d0d417a21031bedcf
lelthar <geraldm1998@hotmail.com>
2018-09-11 23:32:31 -0600

    Se agregó el escritor de txt

--- ModelajeSolid/src/Modelo/EscritorTXT.java
+++ ModelajeSolid/src/Modelo/EscritorTXT.java
@@ -5,6 +5,17 @@
  */
 package Modelo;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author josed
@@ -12,7 +23,24 @@ package Modelo;
 public class EscritorTXT  implements iEscritor {
     
     public boolean Escribir(String Dto){
-        return true;
+        //Obtiene la fecha actual en el sistema
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");  
+        Date date = new Date();  
+        Writer writer_txt;
+        try {
+            //Inicializa el escritor con el path y nombre del archivo a la variable tipo writer
+            writer_txt = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(formatter.format(date)+".txt")));
+            writer_txt.write(Dto);
+            writer_txt.close();
+            return true;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(EscritorTXT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } catch (IOException ex) {
+            Logger.getLogger(EscritorTXT.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
     }
     
 }
--- ModelajeSolid/src/modelajesolid/ModelajeSolid.java
+++ ModelajeSolid/src/modelajesolid/ModelajeSolid.java
@@ -5,7 +5,17 @@
  */
 package modelajesolid;
 
+import Modelo.EscritorTXT;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -19,7 +29,21 @@ public class ModelajeSolid {
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
-        
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");  
+        Date date = new Date();  
+        Writer writer_txt;
+        try {
+            writer_txt = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(formatter.format(date)+".txt")));
+            writer_txt.write("prueba1");
+            writer_txt.close();
+            //return true;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(EscritorTXT.class.getName()).log(Level.SEVERE, null, ex);
+            //return false;
+        } catch (IOException ex) {
+            Logger.getLogger(EscritorTXT.class.getName()).log(Level.SEVERE, null, ex);
+            //return false;
+        }
      
     }
     
