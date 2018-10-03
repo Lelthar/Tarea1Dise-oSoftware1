06e70d881e8db1bcafcac85b70a115735b956067
skylar80004 <josedavidvm04@hotmail.com>
2018-10-03 15:45:56 -0600

    Alfabetos agregados desde archivos
    Ahora es posible agregar nuevos alfabetos
    Se reconcen los archivos .txt que esten dentro de la carpeta Alfabeto

--- DesignProject1/src/controlador/GestorAlfabeto.java
+++ DesignProject1/src/controlador/GestorAlfabeto.java
@@ -6,9 +6,15 @@
 package controlador;
 
 import comunes.DTOAlgoritmos;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import modelo.Alfabeto;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
+import modelo.Algoritmo;
 
 /**
  *
@@ -19,8 +25,10 @@ public class GestorAlfabeto implements iValidable {
     private ArrayList<Alfabeto> alfabetos; 
 
     public GestorAlfabeto(ArrayList<Alfabeto> alfabetos) {
+        
         this.alfabetos = alfabetos;
         cargarDefault();
+        this.cargarAlfabetos("Alfabetos");
     }   
     
     public ArrayList<Alfabeto> getAlfabetos() {
@@ -46,5 +54,111 @@ public class GestorAlfabeto implements iValidable {
         
         Alfabeto alfabeto = new Alfabeto(0,"Default", simbolos);
         alfabetos.add(alfabeto);
+    }
+    
+    
+    
+    public ArrayList<Character> ObtenerAlfabetoDeArchivo(File file) throws FileNotFoundException, IOException{
+        
+        System.out.println(file.getAbsolutePath());
+        
+        FileReader fileReader = new FileReader(file);
+        BufferedReader br = new BufferedReader(fileReader);
+        
+        ArrayList<Character> alfabeto = new ArrayList<Character>();
+        char simbolo;
+        String line =  br.readLine();
+        
+        while (line != null) {
+            
+
+            for(int i = 0; i < line.length() ; i++){
+                simbolo = line.charAt(i);
+                alfabeto.add(simbolo);
+                
+            }
+            
+            
+            line = br.readLine();
+        }
+        
+        System.out.println("lfabeto" + alfabeto);
+        return alfabeto;
+
+    }
+        
+    
+    public ArrayList<String> cargarAlfabetos(String pckgname){ // Carga todos los alfabetos encontrados en la carpeta Alfabetos
+        
+        
+        
+        
+        try{
+            ArrayList<String> archivosAlfabeto = new ArrayList<>(); 
+
+            File directory=null; 
+            try { 
+              directory = new File(Thread.currentThread().getContextClassLoader().getResource(pckgname.replace('.', '/')).getFile());
+              
+            } 
+            catch(NullPointerException x) {
+                
+              System.out.println("Nullpointer");
+              throw new ClassNotFoundException(pckgname+" does not appear to be a valid package"); 
+            }
+            
+            
+            if(directory.exists()) { 
+
+              String[] files = directory.list(); 
+              File fileAlfabeto;
+              String pathFileAlfabeto ;
+              String pathCarpetaAlfabeto = "src/Alfabetos/" ;
+              ArrayList<String> listaPathsArchivos = new ArrayList<String>();
+              
+              //System.out.println(directory.getAbsolutePath());
+              
+              for(int i=0; i<files.length; i++) { 
+                  
+                  if(files[i].endsWith(".txt")) {
+                      pathFileAlfabeto = pathCarpetaAlfabeto + files[i];
+                      archivosAlfabeto.add(files[i].substring(0,files[i].indexOf('.')));
+                      listaPathsArchivos.add(pathFileAlfabeto);
+                  }
+              }
+              
+              ArrayList<Character> simbolosAlfabeto;
+              String nombreAlfabeto;
+              
+              for(int i = 0 ; i < listaPathsArchivos.size() ; i++){
+                  
+                  
+                  fileAlfabeto = new File(listaPathsArchivos.get(i));
+                  simbolosAlfabeto = this.ObtenerAlfabetoDeArchivo(fileAlfabeto);
+                  nombreAlfabeto = archivosAlfabeto.get(i);
+                  Alfabeto alfabeto = new Alfabeto(i,nombreAlfabeto, simbolosAlfabeto);
+                  alfabetos.add(alfabeto);
+                  
+              }
+              
+                  
+                  
+              
+            } else {
+                
+                System.out.println("Directory does not exist");
+                throw new ClassNotFoundException(pckgname+" does not appear to be a valid package"); 
+            }
+            
+            System.out.println(archivosAlfabeto);
+            return archivosAlfabeto;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+        
+        
     }
 }
